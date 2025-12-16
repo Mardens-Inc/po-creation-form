2c0f84c1951eac6ce9f9ab2df0e6562fbae557b8
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-12-16 12:59:25 -0500

    Refactor `Home` page to include `Sidebar` and tab-based layout using `ErrorBoundary` components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,27 @@
-export default function Home() {
+import {useState} from "react";
+import {Sidebar, SidebarTabs, TabConfig} from "../components/Sidebar.tsx";
+import {ErrorBoundary} from "../components/ErrorBoundry.tsx";
+
+export function Home()
+{
+    const [selectedTab, setSelectedTab] = useState<TabConfig>(SidebarTabs.upload);
     return (
-        <>
-            <h1 className="text-3xl font-bold underline">Home Page</h1>
-        </>
+        <div className={"flex flex-col lg:flex-row w-full"}>
+            <ErrorBoundary><Sidebar selectedTab={selectedTab} onSelectionChange={setSelectedTab}/></ErrorBoundary>
+
+            {/* Form Content */}
+            <ErrorBoundary>
+                <div className={"flex flex-col gap-4 w-full py-12 items-center bg-secondary"}>
+                    <div className={"flex flex-col gap-4 w-[75%]"}>
+                        <p className={"font-accent text-2xl sm:text-3xl md:text-4xl lg:text-5xl uppercase"}>{selectedTab.subtitle}</p>
+                        <p className={"font-headers text-3xl sm:text-4xl md:text-5xl lg:text-6xl uppercase text-primary font-bold -mt-2"}>{selectedTab.title}</p>
+                        <p className={"font-text text-sm sm:text-base"}>{selectedTab.description}</p>
+                        <ErrorBoundary>
+                            {selectedTab.component}
+                        </ErrorBoundary>
+                    </div>
+                </div>
+            </ErrorBoundary>
+        </div>
     );
 }
