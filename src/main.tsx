c42bd3b533a0e48e55a254e55433460ced37a476
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-12-16 12:59:38 -0500

    Wrap `Home` component in `ErrorBoundary` and add `ScreenSizeProvider` to improve error handling and window dimension management. Adjust main layout styles for better scrolling.

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,19 @@ import ReactDOM from "react-dom/client";
 import $ from "jquery";
 
 import "./css/index.css";
-import Home from "./pages/Home.tsx";
+import {Home} from "./pages/Home.tsx";
 import Titlebar from "./components/Titlebar.tsx";
 import {HeroUIProvider, ToastProvider} from "@heroui/react";
+import {ScreenSizeProvider} from "./providers/ScreenSizeProvider.tsx";
+import {ErrorBoundary} from "./components/ErrorBoundry.tsx";
 
 
 ReactDOM.createRoot($("#root")[0]!).render(
     <React.StrictMode>
         <BrowserRouter>
-            <MainContentRenderer/>
+            <ScreenSizeProvider>
+                <MainContentRenderer/>
+            </ScreenSizeProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
@@ -36,10 +40,10 @@ export function MainContentRenderer()
             <main className={"flex flex-col p-0 m-0"}>
                 <Titlebar/>
 
-                <div className={"flex flex-row w-full max-h-[calc(100vh-2.5rem)] h-screen overflow-y-hidden p-0 m-0"}>
+                <div className={"flex flex-row w-full max-h-[calc(100vh-2rem)] h-screen overflow-y-auto p-0 m-0"}>
                     <Routes>
                         <Route>
-                            <Route path="/" element={<Home/>}/>
+                            <Route path="/" element={<ErrorBoundary><Home/></ErrorBoundary>}/>
                         </Route>
                     </Routes>
                 </div>
