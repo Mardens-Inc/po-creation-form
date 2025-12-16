8d05ed99c63e49fad632a7cabc2fdbc4a1e10f28
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-12-16 12:56:18 -0500

    Add `zustand` state management and `drag-drop` dependency; initialize form data store
    - Introduced `zustand` for state management with a new `useFormDataStore` in `useFormDataStore.ts`.
    - Added `drag-drop` package for file handling.
    - Updated `pnpm-lock.yaml` and `package.json` to include new dependencies.
    - Created `pnpm-workspace.yaml` for workspace dependency management.

---
+++ src/stores/useFormDataStore.ts
@@ -0,0 +1,22 @@
+import {create} from "zustand";
+import {UploadManifestFormData} from "../components/forms/UploadManifestForm.tsx";
+import {addToast} from "@heroui/react";
+
+export type FormDataStore = {
+    uploadForm: UploadManifestFormData;
+}
+
+type FormDataActions = {
+    setUploadForm: (data: UploadManifestFormData) => void;
+    saveToFile: (data: FormDataStore) => void;
+    loadFromFile: (file: string) => void;
+}
+
+export const useFormDataStore = create<FormDataStore & FormDataActions>((set) => ({
+    uploadForm: {
+        files: []
+    },
+    setUploadForm: (data: UploadManifestFormData) => set(() => ({uploadForm: data})),
+    saveToFile: (_data: FormDataStore) => addToast({title: "Error", description: "This feature is not yet implemented.", color: "danger"}) /* Placeholder for future implementation */,
+    loadFromFile: (_file: string) => addToast({title: "Error", description: "This feature is not yet implemented.", color: "danger"}) /* Placeholder for future implementation */
+}));
