c900a2f5b0f0f2aa49df3ee8859d3d134ff11c2f
Drew Chase (Work) <dcmanproductions@gmail.com>
2026-01-21 11:11:07 -0500

    Remove test endpoint and update `pnpm-lock.yaml` dependencies to latest versions.

--- crates/web/src-actix/main.rs
+++ crates/web/src-actix/main.rs
@@ -1,4 +1,4 @@
 #[actix_web::main]
 async fn main()->anyhow::Result<()>{
-	dashboard_lib::run().await
+	po_creation_dashboard_lib::run().await
 }
---
+++ crates/web/src-actix/util/mod.rs
@@ -0,0 +1,3 @@
+
+pub mod asset_endpoint;
+pub mod http_error;
