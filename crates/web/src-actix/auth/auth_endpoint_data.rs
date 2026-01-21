885b660632086bf667eaaa56fd87be22f1cfeaa1
Drew Chase (Work) <dcmanproductions@gmail.com>
2026-01-21 15:46:46 -0500

    Add `auth_endpoint_data` module: implement `UserRegistrationBody` struct and conversion to `User`

---
+++ crates/web/src-actix/auth/auth_endpoint_data.rs
@@ -0,0 +1,26 @@
+use crate::auth::user_role::UserRole;
+use crate::auth::users_data::User;
+
+#[derive(serde::Deserialize, Debug, Clone)]
+pub struct UserRegistrationBody{
+	#[serde(alias="firstName")]
+	pub first_name: String,
+	#[serde(alias="lastName")]
+	pub last_name: String,
+	pub email: String,
+	pub password: String,
+	pub role: UserRole
+}
+
+impl From<UserRegistrationBody> for User {
+	fn from(val: UserRegistrationBody) -> Self {
+		User {
+			id: None,
+			first_name: val.first_name,
+			last_name: val.last_name,
+			email: val.email,
+			password: val.password,
+			role: val.role,
+		}
+	}
+}
--- crates/web/src-actix/auth/mod.rs
+++ crates/web/src-actix/auth/mod.rs
@@ -2,6 +2,7 @@ mod users_db;
 mod users_data;
 mod user_role;
 mod auth_endpoint;
+mod auth_endpoint_data;
 
 pub use users_db::initialize_table;
 pub use auth_endpoint::configure;
