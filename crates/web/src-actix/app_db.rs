df2140858be5ff964e4dd37f8d54e981ab7311aa
Drew Chase (Work) <dcmanproductions@gmail.com>
2026-01-21 15:19:41 -0500

    Refactor database connection pool: use `MySqlPoolOptions` for configurable pool settings (max/min connections, idle timeout).

--- crates/web/src-actix/app_db.rs
+++ crates/web/src-actix/app_db.rs
@@ -1,14 +1,19 @@
 use sqlx::{ConnectOptions, MySqlPool};
 use anyhow::Result;
-use sqlx::mysql::MySqlConnectOptions;
+use sqlx::mysql::{MySqlConnectOptions, MySqlPoolOptions};
 
 pub async fn create_pool() ->Result<MySqlPool>
 {
 	// The mysql connection string.
-	// For example, `mysql://root:password@localhost:3306/mydatabase`
-	let connection_string = std::env::var("MYSQL_CONNECTION_STRING").expect("DATABASE_URL must be set");
+	// For example, `mysql://root:password@localhost:3306/po_tracker`
+	let connection_string = env!("MYSQL_CONNECTION_STRING");
 
-	let options = connection_string.parse::<MySqlConnectOptions>()?.log_statements(log::LevelFilter::Trace);
-	let connection = MySqlPool::connect_with(options).await?;
+	let connection_options = connection_string.parse::<MySqlConnectOptions>()?.log_statements(log::LevelFilter::Trace);
+	let pool_options = MySqlPoolOptions::new()
+		.max_connections(50)
+		.min_connections(5)
+		.idle_timeout(std::time::Duration::from_secs(3));
+
+	let connection = pool_options.connect_with(connection_options).await?;
 	Ok(connection)
 }
