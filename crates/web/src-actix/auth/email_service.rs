a94cabb08f88c3c352c5f8a38866a164fa248b44
Drew Chase (Work) <dcmanproductions@gmail.com>
2026-01-22 14:56:05 -0500

    Add email service using `lettre` and email templates for account confirmation and password reset

---
+++ crates/web/src-actix/auth/email_service.rs
@@ -0,0 +1,43 @@
+use lettre::{SmtpTransport, Transport};
+use lettre::transport::smtp::authentication::Credentials;
+use log::debug;
+use anyhow::Result;
+use lettre::message::header::ContentType;
+
+const SMTP_HOST: &str = env!("SMTP_HOST");
+const SMTP_USERNAME: &str = env!("SMTP_USERNAME");
+const SMTP_PASSWORD: &str = env!("SMTP_PASSWORD");
+const CONFIRM_EMAIL_TEMPLATE: &str = include_str!("../../templates/confirm_email_template.html.tera");
+const RESET_PASSWORD_TEMPLATE: &str = include_str!("../../templates/reset_password_template.html.tera");
+
+
+
+pub struct EmailService{
+	transport: SmtpTransport,
+}
+
+impl EmailService{
+	pub fn new() -> Result<Self> {
+		debug!("Creating SMTP transport with credentials: {}@{}", SMTP_USERNAME, SMTP_HOST);
+		let creds = Credentials::new(SMTP_USERNAME.to_string(), SMTP_PASSWORD.to_string());
+		let transport = SmtpTransport::starttls_relay(SMTP_HOST)?.credentials(creds).build();
+		Ok(Self { transport })
+	}
+
+	pub async fn send_confirmation_email(&self, email_address: &str, token: &str, first_name: &str) -> Result<()> {
+		debug!("Sending confirmation email to {}", email_address);
+		let mut context = tera::Context::new();
+		context.insert("email", email_address);
+		context.insert("token", token);
+		context.insert("first_name", first_name);
+		let body = tera::Tera::one_off(CONFIRM_EMAIL_TEMPLATE, &context, true)?;
+		let email = lettre::Message::builder()
+			.from(SMTP_USERNAME.parse()?)
+			.to(email_address.parse()?)
+			.subject("Confirm your email address")
+			.header(ContentType::TEXT_HTML)
+			.body(body)?;
+		self.transport.send(&email)?;
+		Ok(())
+	}
+}
